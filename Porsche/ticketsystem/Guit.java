f7f5eea5ca2eb2220ccce98dc53932840186ab85
lukas <lukas@DESKTOP-Q5BHV9B>
2022-01-12 09:56:45 +0100

    Reinkopiert

--- Porsche/ticketsystem/Guit.java
+++ Porsche/ticketsystem/Guit.java
@@ -1,5 +1,11 @@
 package ticketsystem;
 
 public class Guit {
+	public static void main(String[] args) {
+		Bildschirm unserFenster = new Bildschirm();
+		unserBildschirm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		unserBildschirm.setSize(4000, 200);
+		unserBildschirm.setVisible(true);
 
+}
 }
