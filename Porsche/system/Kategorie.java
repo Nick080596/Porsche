a0307a6e5be34cc9df3f22241743ae94729bd939
nj <nj@macbook-pro-von-jan-niklas.fritz.box>
2022-01-19 11:49:16 +0100

    Weitere Klassen angelegt

---
+++ Porsche/system/Kategorie.java
@@ -0,0 +1,25 @@
+package system;
+
+public class Kategorie {
+
+	public static void main(String[] args) {
+		// Enthält Werkstattaufträge
+		
+		String werkstattauftrag;
+		
+		//Werkstattauftrag anlegen
+		Kategorie(parameter1) {
+			werkstattauftrag = parameter1
+			modelle = //aus class Modelle per Dropdown		
+			
+			Kategorie Reifenwechseln = new Kategorie();
+			Kategorie Motor tauschen = new Kategorie();
+			Kategorie Batterie tauschen = new Kategorie();
+			Kategorie Spoiler anbauen = new Kategorie();
+			//erweiterbar, löschbar, keine Verbindung zu Modellen
+			
+		}
+
+	}
+
+}
---
+++ Porsche/system/Kunde.java
@@ -0,0 +1,19 @@
+package system;
+
+public class Kunde {
+
+	public static void main(String[] args) {
+		// Anlegen des Kunden mit Verlinkung von Name und Modell
+
+		void kundeAnlegen() {
+		
+			//wird fortlaufen aus 4 digits erstellt
+			int Kundenummer;
+			//per Eingabe in Feld
+			string Kundenname;
+			//per Dropdown aus vorhandenen Autos wählbar
+			string modelle;
+			
+	}
+	}
+}
