257dabff508a929a3baa8ee105111ccbfdb9b9d6
nj <nj@macbook-pro-von-jan-niklas.fritz.box>
2022-01-19 11:17:23 +0100

    Idee Ticket erstellen rohcode

--- Porsche/ticketsystem/Guit.java
+++ Porsche/ticketsystem/Guit.java
@@ -1,11 +1,9 @@
-package ticketsystem;
-
 public class Guit {
-	public static void main(String[] args) {
-		Bildschirm unserFenster = new Bildschirm();
-		unserBildschirm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		unserBildschirm.setSize(4000, 200);
-		unserBildschirm.setVisible(true);
+    public static void main(String[] args) {
+        Bildschirm unserFenster = new Bildschirm();
+        unserBildschirm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        unserBildschirm.setSize(4000, 200);
+        unserBildschirm.setVisible(true);
 
 }
 }
--- Porsche/ticketsystem/Ticket.java
+++ Porsche/ticketsystem/Ticket.java
@@ -2,4 +2,25 @@ package ticketsystem;
 
 public class Ticket {
 
+	int ticketnummer;
+	String name;
+	String kategorie;
+
+	//Tickenummer automatisch erzeugen, immer weiter erstellen 
+	public Ticket(int ticketnummer) {
+		if (ticketnummer <= 0) {
+			System.exit(0);
+		}
+		this.ticketnummer = ticketnummer;
+	}
+
+	public  name() {
+//Eingabe Name
+		;
+	}
+
+	public kategorie() {
+//Auswählen	der Kategorie
+	}
 }
+	
